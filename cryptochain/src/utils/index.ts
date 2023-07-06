0c345b05408bb2a44276edf367e983f8a2dfebec
ellrussbest <ellrussbest@gmail.com>
2023-07-06 10:41:41 +0200

    changing named paths

--- cryptochain/src/utils/index.ts
+++ cryptochain/src/utils/index.ts
@@ -1,4 +1,4 @@
 export * from "./utils";
 export { default as cryptoHash } from "./crypto-hash";
-export { default as ec } from "./elliptic-curve";
-export * from "./elliptic-curve"
+export { default as ec } from "./elliptic-cryptography";
+export * from "./elliptic-cryptography"
