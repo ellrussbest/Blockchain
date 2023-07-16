1c5bf4d47bb638868ddabec961e6808f0396d2f4
ellrussbest <72700083+ellrussbest@users.noreply.github.com>
2023-07-16 13:35:06 +0200

    Delete cryptochain/dist directory

--- cryptochain/dist/transaction-miner/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TransactionMiner = void 0;
-var transaction_miner_1 = require("./transaction-miner");
-Object.defineProperty(exports, "TransactionMiner", { enumerable: true, get: function () { return __importDefault(transaction_miner_1).default; } });
